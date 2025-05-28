32e31cd472436f83c4bfed1715e9cc5177c7e97f
IVAN FALCON <ivan.falcon@elektra.com.mx>
2025-05-28 13:22:21 -0500

    feat: add csso to minify the styles and correct the export

--- common/packages/common-styles/build.ts
+++ common/packages/common-styles/build.ts
@@ -1,20 +1,14 @@
-// const { spawn } = Bun;
+import { minify } from "csso";
 
-// spawn(["bun", "run", "sass", "src/scss/main.scss:dist/main.css"], {
-//   stdout: "inherit",
-//   stderr: "inherit",
-// });
+const { spawnSync, file, write } = Bun;
 
-// Bun does not have readFile/writeFile, use Bun.file and Bun.write instead
-
-// Rutas
 const sassInput = "src/scss/main.scss";
 const cssOutput = "dist/main.css";
 const resetCSS = "node_modules/@unocss/reset/normalize.css";
 const finalBundle = "dist/bundle.css";
 
 // 1. Compilar Sass a CSS
-const sass = Bun.spawnSync(["sass", `${sassInput}:${cssOutput}`], {
+const sass = spawnSync(["sass", `${sassInput}:${cssOutput}`], {
   stdout: "inherit",
   stderr: "inherit",
 });
@@ -23,11 +17,18 @@ if (sass.success) {
   console.log("✅ Sass compilado correctamente.");
 
   // 2. Leer reset.css y styles.css
-  const reset = await Bun.file(resetCSS).text();
-  const styles = await Bun.file(cssOutput).text();
+  const reset = await file(resetCSS).text();
+  const styles = await file(cssOutput).text();
+
+  // 3. Concatenar CSS
+  const combined = `${reset}\n\n${styles}`;
+
+  // 4. Minificar usando csso
+  const minified = minify(combined).css;
 
   // 3. Concatenar y guardar como bundle
-  await Bun.write(finalBundle, `${reset}\n\n${styles}`);
+  await write(finalBundle, minified);
+
   console.log("✅ Bundle generado: dist/bundle.css");
 } else {
   console.error("❌ Error al compilar Sass.");
