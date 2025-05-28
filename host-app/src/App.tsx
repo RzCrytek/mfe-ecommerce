e7d6eeb1da90e65e739c308d6d9362402bfadabd
IVAN FALCON <ivan.falcon@elektra.com.mx>
2025-05-28 13:20:28 -0500

    feat: switch to sass in mfe and add common-style for base styles

--- host-app/src/App.tsx
+++ host-app/src/App.tsx
@@ -1,6 +1,6 @@
-import './App.css';
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.scss';
 
 const HomePage = lazy(() => import('remoteHome/HomePage'));
 const ProductsPage = lazy(() => import('remoteProducts/ProductsPage'));
