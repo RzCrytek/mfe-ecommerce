fb6ca7520e031ca5b39ca6e05a799c7e8931016f
IVAN FALCON <ivan.falcon@elektra.com.mx>
2025-06-04 11:22:57 -0500

    feat: update configuration for docker with .env

--- mfe-products/src/firebase/firebaseConfig.ts
+++ mfe-products/src/firebase/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_FIREBASE_APIKEY,
-  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECTID,
-  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APP_FIREBASE_APPID,
+  apiKey: process.env.VITE_APP_FIREBASE_APIKEY,
+  authDomain: process.env.VITE_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.VITE_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.VITE_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.VITE_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.VITE_APP_FIREBASE_APPID,
 };
 
 // Initialize Firebase
--- mfe-products/vite.config.ts
+++ mfe-products/vite.config.ts
@@ -1,34 +1,44 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import federation from '@originjs/vite-plugin-federation';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'products',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './ProductsPage': './src/App.tsx',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()); // sin tercer argumento para incluir VITE_
+
+  return {
+    define: {
+      'process.env': env,
+    },
+    plugins: [
+      react(),
+      federation({
+        name: 'products',
+        filename: 'remoteEntry.js',
+        exposes: {
+          './ProductsPage': './src/App.tsx',
+        },
+        shared: ['react', 'react-dom', 'react-router-dom', 'styled-components', 'common-utils'],
+      }),
+    ],
+    server: {
+      port: 5002,
+      host: '0.0.0.0',
+      strictPort: true,
+    },
+    preview: {
+      port: 5002,
+      host: '0.0.0.0',
+      strictPort: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
       },
-      shared: ['react', 'react-dom', 'react-router-dom', 'styled-components', 'common-utils'],
-    }),
-  ],
-  server: {
-    port: 5002,
-    host: '0.0.0.0',
-    strictPort: true,
-  },
-  preview: {
-    port: 5002,
-    host: '0.0.0.0',
-    strictPort: true,
-  },
-  build: {
-    modulePreload: false,
-    target: 'esnext',
-    minify: false,
-    cssCodeSplit: false,
-  },
+    },
+    build: {
+      modulePreload: false,
+      target: 'esnext',
+      minify: false,
+      cssCodeSplit: false,
+    },
+  };
 });
